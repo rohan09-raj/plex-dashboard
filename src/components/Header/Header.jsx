57844e4e29eba1293722ca19f82474f023be9030
rohan09-raj <rajrohan1914@gmail.com>
2023-12-23 16:41:18 +0530

    Modified: cards

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import styles from './Header.module.css'
 
 const Header = () => {
     return (
         <div className={styles.header__container}>
-            <h1 className={styles.header__title}>Pi Dashboard</h1>
+            <h1 className={styles.header__title}>Plex Dashboard</h1>
         </div>
     )
 }
